8fd2d7d807db9c1a4b994ac58bfa02f5db9583ef
Muhammadshoh Rajabov <rajabovmuhammadshoh009@gmail.com>
2022-11-01 03:10:07 +0500

    init: start online-zoo project

---
+++ pages/main/script.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// function sum(a, b, c = 9) {
+// 	return a + b + c
+// }
+// console.log(sum(2, 3))
+
+// let multiplication = (first = 1, second = 2) => {
+// 	return first * second
+// }
+// console.log(multiplication(3, 5))
+
+// let myFunction = function () {
+// 	console.log('hi there')
+// }
+// myFunction = 'let it be strign'
+// myFunction()
+
+// console.log(isNaN(NaN))
+// console.log(isNaN(true))
+// console.log(isNaN('NaN'))
+// console.log(NaN === NaN)
+
+// console.log(typeof parseInt(100))
+// console.log(typeof parseInt('100km'))
+// console.log(parseInt('$100km'))
+// console.log(parseInt('180.655.2384'))
+// console.log(parseFloat('180.655.2384'))
+
+// console.log('My first name is Bob'.toUpperCase())
+
+// const secretPhrase = 'It is a secret'
+
+// console.log(secretPhrase[6])
+// console.log(secretPhrase[secretPhrase.length - 3])
+// console.log(secretPhrase[-3])
+// console.log(secretPhrase.charAt(10))
+// console.log(secretPhrase.indexOf('P'))
+
+// const testnumber = 123
+// console.log(testnumber.toString(), typeof testnumber.toString())
+
+// function firstFunction() {
+// 	let a = 9
+// }
+// firstFunction()
+// // console.log(typeof a)
+// console.log(typeof sam)
+
+// function getMiddle(s) {
+// 	if (s.length == 1 || s == '') return s
+// 	else if (s.length % 2 === 0) {
+// 		return s[s.length / 2 - 1] + s[s.length / 2]
+// 	} else if (s.length % 2 !== 0) {
+// 		return s[Math.floor(s.length / 2)]
+// 	}
+// }
+
+// console.log(getMiddle('test'))
+// console.log(getMiddle('testing'))
+// console.log(getMiddle('middle'))
+// console.log(getMiddle('A'))
+
+// console.log(7 / 2)
